ae676cece5e5c1714712ceacaf641bff153f6c2b
Azamat0321 <dhxndb@gmail.com>
2021-11-11 02:55:19 +0500

    Implement DbStorageService

--- Services/DbStorageService.cs
+++ Services/DbStorageService.cs
@@ -1,7 +1,35 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using tasks.Data;
+
 namespace tasks.Services
 {
-    public class DbStorageService
+    public class DbStorageService : IStorageService
     {
-        
+        private readonly TaskDbContext _context;
+        private readonly ILogger<DbStorageService> _logger;
+
+        public DbStorageService(TaskDbContext context, ILogger<DbStorageService> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        public async Task<(bool IsSuccess, Exception exeption)> InsertTask(Entity.Task task)
+        {
+            try
+            {
+                _context.Tasks.Add(task);
+                await _context.SaveChangesAsync();
+
+                _logger.LogInformation($"Task inserted in DB: {task.Id}");
+                return (true, null);
+            }
+            catch(Exception e)
+            {
+                _logger.LogInformation($"Inserting task to DB failed: {e.Message}",e);
+                return (false, e);
+            }
+        }
     }
 }
