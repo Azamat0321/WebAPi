cf1b67a55c283def33e5a8826e3205d84da478d9
Azamat0321 <dhxndb@gmail.com>
2021-11-10 20:14:23 +0500

    Migrations and Db

--- Data/TaskDbContext.cs
+++ Data/TaskDbContext.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using tasks.Entity;
+
 namespace tasks.Data
 {
-    public class TaskDbContext
+    public class TaskDbContext : DbContext
     {
+        public DbSet<Task> Tasks { get; set; }
+
+        public TaskDbContext(DbContextOptions options)
+            : base(options) { }
         
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using tasks.Data;
 
 namespace tasks
 {
@@ -26,6 +21,9 @@ namespace tasks
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<TaskDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("TaskConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
